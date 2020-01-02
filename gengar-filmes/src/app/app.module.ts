63038e600a7723e54ba6be1f3578c039cd906d79
Paulo Manoel <paulo.manoel@iteris.com.br>
2020-01-02 14:40:58 -0300

    for e carousel

--- gengar-filmes/src/app/app.module.ts
+++ gengar-filmes/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilmesComponent } from './filmes/filmes/filmes.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmesComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
