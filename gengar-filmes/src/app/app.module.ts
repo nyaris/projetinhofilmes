62795a6dec732da25ca499a2a6903e9587c308cf
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-01-03 11:14:47 -0300

    Integração do projeto

--- gengar-filmes/src/app/app.module.ts
+++ gengar-filmes/src/app/app.module.ts
@@ -1,22 +1,52 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FilmesComponent } from './filmes/filmes/filmes.component';
+import { ListaFilmesComponent } from './filmes/lista-filmes/lista-filmes.component';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NovoFilmeComponent } from './filmes/novo-filme/novo-filme.component'
+import { UserHomeComponent } from './page-layouts/user-home/user-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    CarouselComponent,
+    DashboardComponent,
+    FilmesComponent,
+    ListaFilmesComponent,
+    LoginComponent,
+    NovoFilmeComponent,
+    UserHomeComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTableModule,
+    ReactiveFormsModule
   ],
+  entryComponents: [NovoFilmeComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
